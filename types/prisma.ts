274b4c59611dc883eb846a5293cc21fe1470490e
aman <amankhanna224466@gmail.com>
2025-02-02 14:44:50 +0530

    deployment

---
+++ types/prisma.ts
@@ -0,0 +1,28 @@
+import * as runtime from '@prisma/client/runtime/library.js';
+import $Public = runtime.Types.Public
+import $Result = runtime.Types.Result
+import { Prisma } from '@prisma/client';
+
+export type PrismaPromise<T> = $Public.PrismaPromise<T>
+
+
+/**
+ * Model User
+ * 
+ */
+export type User = $Result.DefaultSelection<Prisma.$UserPayload>
+/**
+ * Model Account
+ * 
+ */
+export type Account = $Result.DefaultSelection<Prisma.$AccountPayload>
+/**
+ * Model Transaction
+ * 
+ */
+export type Transaction = $Result.DefaultSelection<Prisma.$TransactionPayload>
+/**
+ * Model Budget
+ * 
+ */
+export type Budget = $Result.DefaultSelection<Prisma.$BudgetPayload>
